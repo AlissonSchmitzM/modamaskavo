e4d2c09a5ee56e1c5be960edbc2984152507e794
Alisson Schmitz <alissonschmitz2@hotmail.com>
2025-07-13 18:13:26 -0300

    Aplicando ultimas correcoes para EAS build (babel.config.js e package-lock.json)

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,9 @@
-const { getDefaultConfig } = require('metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-    },
-  };
-})();
+const config = {
+  resolver: {
+    unstable_enableSymlinks: false,
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
