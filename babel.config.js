1980774c986d89b1f1be39d805253098747dc8c1
Alisson Schmitz <alissonschmitz2@hotmail.com>
2025-07-13 18:03:39 -0300

    Aplicando ultimas correcoes para EAS build (babel.config.js e package-lock.json)

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
-module.exports = function (api) {
-  api.cache(true); // Boa prática para caching
+module.exports = function(api) {
+  api.cache(true);
   return {
-    presets: ['babel-preset-expo'], // <<< Mude para este preset
+    presets: ['babel-preset-expo'], // Ou '@babel/preset-env', '@react-native/babel-preset'
+    plugins: [
+      'react-native-reanimated/plugin', // Essencial para Reanimated
+      // Outros plugins que você possa ter (ex: para módulos de estilo, etc.)
+    ],
   };
 };
